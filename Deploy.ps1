adabbccc61aa5aa975f3538f4e020d08d3bda039
Lyrana Hughes <lyhughes@microsoft.com>
2020-06-25 13:08:49 -0700

    hello script

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,35 +1,31 @@
-﻿$subscription = "05aea7c9-25e9-4b5d-aeb5-fd0713992c6b"
-$oid = "ecfc380a-1ab2-463c-bdd3-9550ce8f2bee"
-$numbEnv = 3
-$numbInNameToStartAt = 1
-$environmentName = "bugbashloop"
+﻿$subscription = $args[0]
+$environmentName = $args[2]
 $deploymentNamePrefix = $environmentName + "deployment"
-$resourceGroupName = "powerShellTesting"
-$pathToTemplate = "C:\Repos\TsiArmTemplates\WithIoTHub\deploy.json"
-$pathToParams = "C:\Repos\TsiArmTemplates\WithIoTHub\params.json"
+$resourceGroupName = $args[1]
+$pathToTemplate = $args[6]
+$pathToParams = $args[5]
 
 $tsID = @(
-       [pscustomobject]@{name='iothub-connection-device-id';type='string'}
+       [pscustomobject]@{name=$args[3] ;type='string'}
    )
+
+# Connect-AzAccount
 
 Set-AzContext -SubscriptionId $subscription
 
-For ($i=0; $i -le $numbEnv; $i++) {
-
-
 $a = Get-Content $pathToParams | ConvertFrom-Json
 $a.parameters.environmentTimeSeriesIdProperties.value = $tsID
-$a.parameters.iotHubName.value = $environmentName + "hub" + $i
-$a.parameters.environmentName.value = $environmentName + $i
-$a.parameters.eventSourceName.value = $environmentName + $i + "eventSource"
+$a.parameters.iotHubName.value = $environmentName + "hub"
+$a.parameters.environmentName.value = $environmentName
+$a.parameters.eventSourceName.value = $environmentName + "eventSource"
+$a.parameters.eventSourceTimestampPropertyName.value = $args[4]
 
 $a | ConvertTo-Json -depth 32| set-content $pathToParams
 
 
 New-AzResourceGroupDeployment -Name ($deploymentNamePrefix -join $i) -ResourceGroupName $resourceGroupName -TemplateFile  $pathToTemplate `
   -TemplateParameterFile $pathToParams `
-  -AsJob
-}
+
 
 
 
